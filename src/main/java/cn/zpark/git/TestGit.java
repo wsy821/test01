5e315e37a431a87a538a61650d413003487ea25c
wsy <wsy2108@126.com>
2025-05-21 12:15:07 +0800

    第一次提交

---
+++ src/main/java/cn/zpark/git/TestGit.java
@@ -0,0 +1,13 @@
+package cn.zpark.git;
+
+/**
+ * @Auther:BigData-wsy
+ * @ClassName:TestGit
+ * @功能描述:
+ * @Version:1.0
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("wsy");
+    }
+}
